786ca30f0dc7095e68ff917fe35e93fc8c88045e
Sophie Collard <sophie.collard@hotmail.fr>
2020-09-21 18:15:58 +0100

    Expand EitherT syntax package

--- backend/src/main/scala/com/github/sophiecollard/bookswap/syntax/EitherTSyntax.scala
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/syntax/EitherTSyntax.scala
@@ -5,6 +5,11 @@ import cats.data.{EitherT, OptionT}
 
 object EitherTSyntax {
 
+  implicit class FEitherToEitherT[F[_], E, A](value: F[Either[E, A]]) {
+    def asEitherT: EitherT[F, E, A] =
+      EitherT(value)
+  }
+
   implicit class FOpToEitherT[F[_], A](value: F[Option[A]]) {
     def asEitherT[E](left: => E)(implicit F: Functor[F]): EitherT[F, E, A] =
       OptionT(value).toRight(left)
