b0ede8b299f1a83eaf4bdc3512f5f623e1126197
Sophie Collard <sophie.collard@hotmail.fr>
2020-09-24 19:47:07 +0100

    Implement user repository

--- backend/src/main/scala/com/github/sophiecollard/bookswap/domain/user/UserStatus.scala
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/domain/user/UserStatus.scala
@@ -1,12 +1,12 @@
 package com.github.sophiecollard.bookswap.domain.user
 
-import enumeratum.values.{StringEnum, StringEnumEntry}
+import enumeratum.values.{StringDoobieEnum, StringEnum, StringEnumEntry}
 
 import scala.collection.immutable
 
 sealed abstract class UserStatus(val value: String) extends StringEnumEntry
 
-object UserStatus extends StringEnum[UserStatus] {
+object UserStatus extends StringEnum[UserStatus] with StringDoobieEnum[UserStatus] {
 
   case object PendingVerification extends UserStatus("pending_verification")
   case object Active              extends UserStatus("active")
--- backend/src/main/scala/com/github/sophiecollard/bookswap/repositories/user/UserRepository.scala
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/repositories/user/UserRepository.scala
@@ -2,9 +2,28 @@ package com.github.sophiecollard.bookswap.repositories.user
 
 import com.github.sophiecollard.bookswap.domain.shared.Id
 import com.github.sophiecollard.bookswap.domain.user.User
+import doobie.{ConnectionIO, Query0}
+import doobie.implicits._
 
 trait UserRepository[F[_]] {
 
   def get(id: Id[User]): F[Option[User]]
+
+}
+
+object UserRepository {
+
+  def create: UserRepository[ConnectionIO] = new UserRepository[ConnectionIO] {
+    override def get(id: Id[User]): ConnectionIO[Option[User]] =
+      getQuery(id)
+        .option
+  }
+
+  def getQuery(id: Id[User]): Query0[User] =
+    sql"""
+         |SELECT id, name, status
+         |FROM users
+         |WHERE id = $id
+       """.query[User]
 
 }
