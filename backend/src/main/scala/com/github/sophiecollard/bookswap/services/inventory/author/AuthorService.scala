5d0bf1a7dbcb04063c5e45ba7a2a574141c29851
Sophie Collard <sophie.collard@hotmail.fr>
2020-09-26 16:31:26 +0100

    Refactor author service

--- backend/src/main/scala/com/github/sophiecollard/bookswap/services/inventory/author/AuthorService.scala
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/services/inventory/author/AuthorService.scala
@@ -1,9 +1,12 @@
 package com.github.sophiecollard.bookswap.services.inventory.author
 
-import cats.~>
+import cats.{Functor, ~>}
+import cats.implicits._
 import com.github.sophiecollard.bookswap.domain.inventory.Author
 import com.github.sophiecollard.bookswap.domain.shared.Id
 import com.github.sophiecollard.bookswap.domain.user.User
+import com.github.sophiecollard.bookswap.error.Error.TransactionError.ResourceNotFound
+import com.github.sophiecollard.bookswap.error.Error.{TransactionError, TransactionErrorOr}
 import com.github.sophiecollard.bookswap.repositories.inventory.AuthorRepository
 import com.github.sophiecollard.bookswap.services.authorization.Instances._
 import com.github.sophiecollard.bookswap.services.authorization._
@@ -11,7 +14,7 @@ import com.github.sophiecollard.bookswap.services.syntax._
 
 trait AuthorService[F[_]] {
 
-  def get(id: Id[Author]): F[Option[Author]]
+  def get(id: Id[Author]): F[TransactionErrorOr[Author]]
 
   def delete(id: Id[Author])(userId: Id[User]): F[WithAuthorizationByAdminStatus[Unit]]
 
@@ -19,14 +22,17 @@ trait AuthorService[F[_]] {
 
 object AuthorService {
 
-  def create[F[_], G[_]](
+  def create[F[_], G[_]: Functor](
     authorizationService: AuthorizationService[F, Id[User], ByAdminStatus],
     repository: AuthorRepository[G],
     transactor: G ~> F
   ): AuthorService[F] =
     new AuthorService[F] {
-      override def get(id: Id[Author]): F[Option[Author]] =
-        repository.get(id).transact(transactor)
+      override def get(id: Id[Author]): F[TransactionErrorOr[Author]] =
+        repository
+          .get(id)
+          .map(_.toRight[TransactionError](ResourceNotFound("Author", id)))
+          .transact(transactor)
 
       override def delete(id: Id[Author])(userId: Id[User]): F[WithAuthorizationByAdminStatus[Unit]] =
         authorizationService.authorize(userId) {
