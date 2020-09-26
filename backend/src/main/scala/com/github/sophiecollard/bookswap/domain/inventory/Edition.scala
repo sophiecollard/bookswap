b0461beea2ac988f2a2b416eb9a73f791fb02ef9
Sophie Collard <sophie.collard@hotmail.fr>
2020-09-26 16:13:35 +0100

    Update Edition class

--- backend/src/main/scala/com/github/sophiecollard/bookswap/domain/inventory/Edition.scala
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/domain/inventory/Edition.scala
@@ -3,11 +3,12 @@ package com.github.sophiecollard.bookswap.domain.inventory
 import java.time.LocalDate
 
 import cats.data.NonEmptyList
+import com.github.sophiecollard.bookswap.domain.shared.Id
 
 final case class Edition(
   isbn: ISBN,
   title: Title,
-  authors: NonEmptyList[Author],
-  publisher: Option[Publisher],
+  authorIds: NonEmptyList[Id[Author]],
+  publisherId: Option[Id[Publisher]],
   publicationDate: Option[LocalDate]
 )
