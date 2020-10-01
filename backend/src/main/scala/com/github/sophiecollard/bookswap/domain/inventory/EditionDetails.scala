b4814e149dc555b82a83e394afe83491b6e7605d
Sophie Collard <sophie.collard@hotmail.fr>
2020-10-01 18:01:56 +0100

    Add create and update methods to the edition repository

---
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/domain/inventory/EditionDetails.scala
@@ -0,0 +1,13 @@
+package com.github.sophiecollard.bookswap.domain.inventory
+
+import java.time.LocalDate
+
+import cats.data.NonEmptyList
+import com.github.sophiecollard.bookswap.domain.shared.Id
+
+final case class EditionDetails(
+  title: Title,
+  authorIds: NonEmptyList[Id[Author]],
+  publisherId: Option[Id[Publisher]],
+  publicationDate: Option[LocalDate]
+)
--- backend/src/main/scala/com/github/sophiecollard/bookswap/repositories/inventory/EditionRepository.scala
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/repositories/inventory/EditionRepository.scala
@@ -1,38 +1,68 @@
 package com.github.sophiecollard.bookswap.repositories.inventory
 
-import com.github.sophiecollard.bookswap.domain.inventory.{Edition, ISBN}
+import com.github.sophiecollard.bookswap.domain.inventory.{Edition, EditionDetails, ISBN}
 import com.github.sophiecollard.bookswap.implicits.All._
-import doobie.{ConnectionIO, Query0, Update0}
+import doobie.{ConnectionIO, Query0, Update0, Update}
 import doobie.implicits._
 import doobie.implicits.javatime._
 
 trait EditionRepository[F[_]] {
 
-  def get(isbn: ISBN): F[Option[Edition]]
+  /** Creates a new Edition */
+  def create(edition: Edition): F[Unit]
 
+  /** Updates the details of the specified Edition */
+  def update(isbn: ISBN, details: EditionDetails): F[Unit]
+
+  /** Deletes the specified Edition */
   def delete(isbn: ISBN): F[Unit]
+
+  /** Returns the specified Edition */
+  def get(isbn: ISBN): F[Option[Edition]]
 
 }
 
 object EditionRepository {
 
   def create: EditionRepository[ConnectionIO] = new EditionRepository[ConnectionIO] {
-    override def get(isbn: ISBN): ConnectionIO[Option[Edition]] =
-      getQuery(isbn)
-        .option
+    override def create(edition: Edition): ConnectionIO[Unit] =
+      createUpdate
+        .run(edition)
+        .map(_ => ())
+
+    override def update(isbn: ISBN, details: EditionDetails): ConnectionIO[Unit] =
+      updateUpdate(isbn, details)
+        .run
+        .map(_ => ())
 
     override def delete(isbn: ISBN): ConnectionIO[Unit] =
       deleteUpdate(isbn)
         .run
         .map(_ => ())
+
+    override def get(isbn: ISBN): ConnectionIO[Option[Edition]] =
+      getQuery(isbn)
+        .option
   }
 
-  def getQuery(isbn: ISBN): Query0[Edition] =
+  val createUpdate: Update[Edition] =
+    Update[Edition](
+      s"""
+         |INSERT INTO editions (isbn, title, author_ids, publisher_id, publication_date)
+         |VALUES (?, ?, ?, ?, ?)
+         |ON CONFLICT isbn DO NOTHING
+       """.stripMargin
+    )
+
+  def updateUpdate(isbn: ISBN, details: EditionDetails): Update0 =
     sql"""
-         |SELECT isbn, title, author_ids, publisher_id, publication_date
-         |FROM editions
+         |UPDATE editions
+         |SET title = ${details.title},
+         |author_ids = ${details.authorIds},
+         |publisher_id = ${details.publisherId},
+         |publication_date = ${details.publicationDate}
          |WHERE isbn = $isbn
-       """.stripMargin.query[Edition]
+       """.stripMargin.update
 
   def deleteUpdate(isbn: ISBN): Update0 =
     sql"""
@@ -40,5 +70,12 @@ object EditionRepository {
          |FROM editions
          |WHERE isbn = $isbn
        """.stripMargin.update
+
+  def getQuery(isbn: ISBN): Query0[Edition] =
+    sql"""
+         |SELECT isbn, title, author_ids, publisher_id, publication_date
+         |FROM editions
+         |WHERE isbn = $isbn
+       """.stripMargin.query[Edition]
 
 }
