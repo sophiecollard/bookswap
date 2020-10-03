029e39dcaa0d4924f3051d92551728081702f532
Sophie Collard <sophie.collard@hotmail.fr>
2020-10-03 12:49:06 +0100

    Add user service

--- backend/src/main/scala/com/github/sophiecollard/bookswap/services/user/UserService.scala
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/services/user/UserService.scala
@@ -1,9 +1,14 @@
 package com.github.sophiecollard.bookswap.services.user
 
+import cats.{Functor, ~>}
 import com.github.sophiecollard.bookswap.domain.shared.{Id, Name}
-import com.github.sophiecollard.bookswap.domain.user.User
-import com.github.sophiecollard.bookswap.error.Error.ServiceErrorOr
-import com.github.sophiecollard.bookswap.services.authorization.Instances.WithAuthorizationBySelf
+import com.github.sophiecollard.bookswap.domain.user.{User, UserStatus}
+import com.github.sophiecollard.bookswap.error.Error.ServiceError.{FailedToCreateResource, FailedToDeleteResource, ResourceNotFound}
+import com.github.sophiecollard.bookswap.error.Error.{ServiceError, ServiceErrorOr}
+import com.github.sophiecollard.bookswap.repositories.user.UserRepository
+import com.github.sophiecollard.bookswap.services.authorization.AuthorizationService
+import com.github.sophiecollard.bookswap.services.authorization.Instances.{BySelf, WithAuthorizationBySelf}
+import com.github.sophiecollard.bookswap.services.syntax._
 
 trait UserService[F[_]] {
 
@@ -15,5 +20,39 @@ trait UserService[F[_]] {
 
   /** Invoked by a User to delete itself */
   def delete(id: Id[User])(userId: Id[User]): F[WithAuthorizationBySelf[ServiceErrorOr[Unit]]]
+
+}
+
+object UserService {
+
+  def create[F[_], G[_]: Functor](
+    authorizationBySelf: AuthorizationService[F, (Id[User], Id[User]), BySelf],
+    userRepository: UserRepository[G],
+    transactor: G ~> F
+  ): UserService[F] = new UserService[F] {
+    override def get(id: Id[User]): F[ServiceErrorOr[User]] =
+      userRepository
+        .get(id)
+        .orElse[ServiceError](ResourceNotFound("User", id))
+        .transact(transactor)
+
+    override def create(name: Name[User]): F[ServiceErrorOr[User]] = {
+      val user = User(id = Id.generate[User], name, status = UserStatus.PendingVerification)
+      userRepository
+        .create(user)
+        .ifTrue(user)
+        .orElse[ServiceError](FailedToCreateResource("User", user.id))
+        .transact(transactor)
+    }
+
+    override def delete(id: Id[User])(userId: Id[User]): F[WithAuthorizationBySelf[ServiceErrorOr[Unit]]] =
+      authorizationBySelf.authorize((id, userId)) {
+        userRepository
+          .delete(id)
+          .ifTrue(())
+          .orElse[ServiceError](FailedToDeleteResource("User", id))
+          .transact(transactor)
+      }
+  }
 
 }
