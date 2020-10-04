76430a361bca879aaaffc1ff21b8621636e1af20
Sophie Collard <sophie.collard@hotmail.fr>
2020-10-04 16:21:10 +0100

    Define safe constructor for ISBN domain type

--- backend/src/main/scala/com/github/sophiecollard/bookswap/domain/inventory/ISBN.scala
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/domain/inventory/ISBN.scala
@@ -6,6 +6,13 @@ sealed abstract case class ISBN(value: String)
 
 object ISBN {
 
+  def apply(value: String): Option[ISBN] = {
+    val isbnPattern = "^[0-9]{13}$".r
+    isbnPattern findFirstIn value map {
+      new ISBN(_) {}
+    }
+  }
+
   def unvalidated(value: String): ISBN =
     new ISBN(value) {}
 
