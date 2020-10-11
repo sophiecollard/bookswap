296f25f659fa96cdfe9082dfac49ef6eea587801
Sophie Collard <sophie.collard@hotmail.fr>
2020-10-11 12:39:21 +0100

    Refactor RequestStatus type

--- backend/src/main/scala/com/github/sophiecollard/bookswap/domain/transaction/RequestStatus.scala
+++ backend/src/main/scala/com/github/sophiecollard/bookswap/domain/transaction/RequestStatus.scala
@@ -2,15 +2,26 @@ package com.github.sophiecollard.bookswap.domain.transaction
 
 import java.time.LocalDateTime
 
-import doobie.util.{Read, Write}
 import doobie.implicits.javatime._
+import doobie.util.{Read, Write}
 import enumeratum.{Enum, EnumEntry}
+import io.circe.syntax._
+import io.circe.{Encoder, Json}
 
 import scala.collection.immutable
 
-sealed trait RequestStatus extends EnumEntry {
+sealed abstract class RequestStatus(val value: String) extends EnumEntry {
 
   import RequestStatus._
+
+  def maybeTimestamp: Option[LocalDateTime] = this match {
+    case Pending                => None
+    case Accepted(acceptedOn)   => Some(acceptedOn)
+    case OnWaitingList(addedOn) => Some(addedOn)
+    case Rejected(rejectedOn)   => Some(rejectedOn)
+    case Fulfilled(fulfilledOn) => Some(fulfilledOn)
+    case Cancelled(cancelledOn) => Some(cancelledOn)
+  }
 
   def isPending: Boolean = this match {
     case Pending => true
@@ -46,14 +57,21 @@ sealed trait RequestStatus extends EnumEntry {
 
 object RequestStatus extends Enum[RequestStatus] {
 
-  case object Pending                                    extends RequestStatus
-  final case class Accepted(acceptedOn: LocalDateTime)   extends RequestStatus
-  final case class OnWaitingList(addedOn: LocalDateTime) extends RequestStatus
-  final case class Rejected(rejectedOn: LocalDateTime)   extends RequestStatus
-  final case class Fulfilled(fulfilledOn: LocalDateTime) extends RequestStatus
-  final case class Cancelled(cancelledOn: LocalDateTime) extends RequestStatus
+  case object Pending                                    extends RequestStatus(PendingStatusValue)
+  final case class Accepted(acceptedOn: LocalDateTime)   extends RequestStatus(AcceptedStatusValue)
+  final case class OnWaitingList(addedOn: LocalDateTime) extends RequestStatus(OnWaitingListStatusValue)
+  final case class Rejected(rejectedOn: LocalDateTime)   extends RequestStatus(RejectedStatusValue)
+  final case class Fulfilled(fulfilledOn: LocalDateTime) extends RequestStatus(FulfilledStatusValue)
+  final case class Cancelled(cancelledOn: LocalDateTime) extends RequestStatus(CancelledStatusValue)
 
   override val values: immutable.IndexedSeq[RequestStatus] = findValues
+
+  val PendingStatusValue: String = "pending"
+  val AcceptedStatusValue: String = "accepted"
+  val OnWaitingListStatusValue: String = "on_waiting_list"
+  val RejectedStatusValue: String = "rejected"
+  val FulfilledStatusValue: String = "fulfilled"
+  val CancelledStatusValue: String = "cancelled"
 
   def pending: RequestStatus = Pending
   def accepted(acceptedOn: LocalDateTime): RequestStatus = Accepted(acceptedOn)
@@ -64,23 +82,26 @@ object RequestStatus extends Enum[RequestStatus] {
 
   implicit val read: Read[RequestStatus] =
     Read[(String, Option[LocalDateTime])].map {
-      case ("pending", None)                  => Pending
-      case ("accepted", Some(acceptedOn))     => Accepted(acceptedOn)
-      case ("on_waiting_list", Some(addedOn)) => OnWaitingList(addedOn)
-      case ("rejected", Some(rejectedOn))     => Rejected(rejectedOn)
-      case ("fulfilled", Some(fulfilledOn))   => Fulfilled(fulfilledOn)
-      case ("cancelled", Some(cancelledOn))   => Cancelled(cancelledOn)
+      case (PendingStatusValue, None)                => Pending
+      case (AcceptedStatusValue, Some(acceptedOn))   => Accepted(acceptedOn)
+      case (OnWaitingListStatusValue, Some(addedOn)) => OnWaitingList(addedOn)
+      case (RejectedStatusValue, Some(rejectedOn))   => Rejected(rejectedOn)
+      case (FulfilledStatusValue, Some(fulfilledOn)) => Fulfilled(fulfilledOn)
+      case (CancelledStatusValue, Some(cancelledOn)) => Cancelled(cancelledOn)
       case other => throw new RuntimeException(s"Invalid RequestStatus: $other")
     }
 
   implicit val write: Write[RequestStatus] =
-    Write[(String, Option[LocalDateTime])].contramap {
-      case Pending                => ("pending", None)
-      case Accepted(acceptedOn)   => ("accepted", Some(acceptedOn))
-      case OnWaitingList(addedOn) => ("on_waiting_list", Some(addedOn))
-      case Rejected(rejectedOn)   => ("rejected", Some(rejectedOn))
-      case Fulfilled(fulfilledOn) => ("fulfilled", Some(fulfilledOn))
-      case Cancelled(cancelledOn) => ("cancelled", Some(cancelledOn))
+    Write[(String, Option[LocalDateTime])].contramap { status =>
+      (status.value, status.maybeTimestamp)
+    }
+
+  implicit val encoder: Encoder[RequestStatus] =
+    Encoder.instance { status =>
+      Json.obj(
+        "status" := status.value,
+        "timestamp" := status.maybeTimestamp.map(_.asJson).getOrElse(Json.Null)
+      )
     }
 
 }
